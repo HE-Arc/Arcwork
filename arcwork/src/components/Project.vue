fb106956953af4b44d981accbbe395bfaa0eed69
matthieu.barbot <matthieu.barbot@he-arc.ch>
2021-11-24 23:14:26 +0100

    add style

--- arcwork/src/components/Project.vue
+++ arcwork/src/components/Project.vue
@@ -1,19 +1,33 @@
 <template>
-    <h1>{{name}} {{ id }}</h1>
+  <div id="test">
+    <h1>{{ name }} {{ id }}</h1>
+    <ul>
+      <li v-for="hashtag in hashtags" :key="hashtag">
+        {{ hashtag }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    props : ['id'],
-    data() {
-        return {
-            name : "projet"
-        }
-    },
+  props: ["id"],
+  data() {
+    return {
+      name: "projet",
+      hashtags: ["go", "linux", "c++"],
+    };
+  },
 
-    created: () => {
-        console.log("yolo")
-    }
-
-}
+  created: () => {
+    console.log("yolo");
+  },
+};
 </script>
+
+
+<style >
+#test {
+  border: 1px solid #eee;
+}
+</style>
