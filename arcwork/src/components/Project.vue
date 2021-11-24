8cac814a10b1b560aee270e77a9047760694ad85
matthieu.barbot <matthieu.barbot@he-arc.ch>
2021-11-24 19:52:53 +0100

    add vue router

---
+++ arcwork/src/components/Project.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{name}} {{ id }}</h1>
+</template>
+
+<script>
+export default {
+    props : ['id'],
+    data() {
+        return {
+            name : "projet"
+        }
+    }
+
+}
+</script>
---
+++ arcwork/src/components/Projects.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+     <h1>all projects ahaha</h1>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
