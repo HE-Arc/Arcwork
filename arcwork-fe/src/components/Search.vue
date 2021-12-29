40806cf66cc6b7cb0be89562f525473828627945
petitBiscuits <loic.frossard01@gmail.com>
2021-12-29 10:36:59 +0100

    feat<#20> Add a Search Bar

---
+++ arcwork-fe/src/components/Search.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-8/12 m-auto">
+        <input class="shadow appearance-none border rounded w-full my-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="message" placeholder="modifiez-moi">
+        <p>Le message est : {{ message }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+        return {
+            message: "Hello",
+        };
+    },
+};
+</script>
