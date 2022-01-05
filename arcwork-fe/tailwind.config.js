ba1dfebbddeaa99a4a05465f8ea06a9417f186d0
petitBiscuits <loic.frossard01@gmail.com>
2022-01-05 15:05:56 +0100

    feat Add some colors

--- arcwork-fe/tailwind.config.js
+++ arcwork-fe/tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'charcoal': '#374151',
+        'mantis' : '#6CC25B',
+        'lavender-web' : '#DADFF7',
+        'dark-side-blue': '#A0C1D1' ,
+        'lavender-gray' : '#B5B2C2',
+      },
+    },
   },
   variants: {
     extend: {},
