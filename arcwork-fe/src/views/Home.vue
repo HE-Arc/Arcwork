14b201f30339b1c2fcd05e3f3d0d584c93b4b21d
petitBiscuits <loic.frossard01@gmail.com>
2022-01-05 15:10:00 +0100

    restore<#30> Add NavBar

--- arcwork-fe/src/views/Home.vue
+++ arcwork-fe/src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
-    <div class="home">
-        <div class="w-1/2 mx-auto">
-            <div class="text-5xl font-bold my-10">Welcome to Arcwork</div>
-            <div class="flex flex-row space-x-7">
-                <Search class="inline-block" placeholder="Email"/>
-                <input class="cursor-pointer bg-green-400 rounded px-4 my-4 hover:bg-green-500" type="button" value="Sign in">
-            </div>
+    <NavBar />
+    <div class="w-1/2 mx-auto">
+        <div class="text-5xl font-bold my-10">Welcome to Arcwork</div>
+        <div class="flex flex-row space-x-7">
+            <Search class="inline-block" placeholder="Email"/>
+            <input class="cursor-pointer bg-green-400 rounded px-4 my-4 hover:bg-green-500" type="button" value="Sign in">
         </div>
         <h1>welcome !</h1>
         <UserMin id="1" />
@@ -16,12 +15,14 @@
 <script>
 import Search from '../components/Search.vue';
 import UserMin from "../components/UserMin";
+import NavBar from "../components/NavBar";
 
 export default {
     name: "Home",
     components: {
         UserMin,
         Search,
+        NavBar,
     },
 };
 </script>
