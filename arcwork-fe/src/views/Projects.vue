25e09033c8c493d3b54dc32154c030ff467c9614
petitBiscuits <loic.frossard01@gmail.com>
2022-01-08 22:14:19 +0100

    style<#8> update du style

--- arcwork-fe/src/views/Projects.vue
+++ arcwork-fe/src/views/Projects.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="projects">
-        <h1>all projects ! {{ name }}</h1>
+    <NavBar />
+    <div class="w-1/2 mx-auto">
+        <div class="flex flex-row space-x-7">
+            <Search class="flex-1" placeholder="Recherche" label="Recherche"/>
+            <input class="cursor-pointer bg-green-400 rounded px-2 my-4 hover:bg-green-500" type="button" value="Rechercher" v-on:click="search"/>
+        </div>
         <div class="flex flex-col space-y-7">
             <ProjectMin id="1" />
             <ProjectMin id="2" />
@@ -10,6 +14,8 @@
 </template>
 
 <script>
+import NavBar from "../components/NavBar";
+import Search from '../components/Search.vue';
 import ProjectMin from "../components/ProjectMin";
 import { getData } from "../tools/network";
 const testData = { name: "matthieu123" };
@@ -18,6 +24,8 @@ export default {
     name: "Projects",
     components: {
         ProjectMin,
+        NavBar,
+        Search,
     },
     data() {
         return {
