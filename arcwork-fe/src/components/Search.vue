b65dd07138f098bcc27c3956f1e2f0afafee6a89
petitBiscuits <loic.frossard01@gmail.com>
2022-01-08 22:16:45 +0100

    style<#9> Add about vue and change style

--- arcwork-fe/src/components/Search.vue
+++ arcwork-fe/src/components/Search.vue
@@ -1,14 +1,17 @@
 <template>
-    <input class="appearance-none rounded my-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" :placeholder="placeholder">
+    <div>
+        <label v-if="label" class="appearance-none rounded my-4 py-2 px-3 text-gray-700 leading-tight text-lg">{{label}}</label>
+        <input class="appearance-none rounded my-4 py-2 px-3 text-gray-700 w-2/3 leading-tight focus:outline-none focus:shadow-outline" :placeholder="placeholder">
+    </div>
 </template>
 
 <script>
 
 export default {
-    props: ["placeholder"],
+    props: ["placeholder","label"],
     /*data() {
         return {
-            message: "Hello",
+            message: "Hello",s
         };
     },*/
 };
